7490760eb16b3947595736ff54822c77e5e529a3
Ivan Panchenko <arigatory@gmail.com>
2024-04-10 23:37:19 +0300

    done

---
+++ app/config/components/security.py
@@ -0,0 +1,34 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+import os
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DEBUG", False) == "True"
+
+# ALLOWED_HOSTS = [i for i in os.environ.get("ALLOWED_HOSTS").split(" ")]
+ALLOWED_HOSTS = [
+    '*'
+]
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
