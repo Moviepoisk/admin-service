2d3fd71e8e57972c8a002b43d380d3ea98730155
Ivan Panchenko <arigatory@gmail.com>
2024-05-10 18:25:57 +0300

    allow all hosts

--- app/config/components/security.py
+++ app/config/components/security.py
@@ -10,7 +10,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = [i for i in os.environ.get("ALLOWED_HOSTS").split(" ")]
+ALLOWED_HOSTS = ['*']
+
+# ALLOWED_HOSTS = [i for i in os.environ.get("ALLOWED_HOSTS").split(" ")]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
