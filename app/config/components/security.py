71055e5d759d26321e78484a734d659e980035cb
Denis Shvetsov <den.shvetsov@yandex.ru>
2024-05-16 08:09:57 +0300

    fix: allowed hosts from env

--- app/config/components/security.py
+++ app/config/components/security.py
@@ -10,9 +10,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = [
+#     '0.0.0.0',
+#     '127.0.0.1',
+#     'localhost,
+#     'https://admin.moviepoisk.ru']
 
-# ALLOWED_HOSTS = [i for i in os.environ.get("ALLOWED_HOSTS").split(" ")]
+ALLOWED_HOSTS = [i for i in os.environ.get("ALLOWED_HOSTS").split(" ")]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
